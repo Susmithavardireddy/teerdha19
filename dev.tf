06a4cc6cb8acf3699df0f9f4b9310a5b021a1412
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-08-07 18:19:13 +0530

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -18,6 +18,7 @@ provisioner "remote-exec" {
         "sudo apt install python3-pip -y",
         "sudo apt install python3-venv -y",
         "sudo apt install python3-virtualenv -y",
+        "pip install virtualenv",
         "python3 -m venv /home/ubuntu/kumar",
         ". /home/ubuntu/kumar/bin/activate",
         "git clone https://github.com/Praveenchinna14/teerdha19.git",
@@ -26,13 +27,11 @@ provisioner "remote-exec" {
         "sudo apt install pkg-config -y",
         "pip install -r requirements.txt",
         "pip install django",
-        "pip install django-crsipy-forms",
         "pip install django-rest-framework",
         "pip install requests",
         "pip install pyscopg2-binary",
         "pip install wheel",
         "pip install pillow",
-        "pip install easy-pil",
         "python3 /home/ubuntu/teerdha19/manage.py makemgrations",
         "python3 /home/ubuntu/teerdha19/manage.py migrate",
         "python3 /home/ubuntu/teerdha19/manage.py runserver 0.0.0.0:8000"     
@@ -45,4 +44,3 @@ provisioner "remote-exec" {
     }
  }
 }
-
