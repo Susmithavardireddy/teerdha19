3078f93a63877a1a6eb05c2150ab9aa3f0a37290
Susmithavardireddy <165268982+Susmithavardireddy@users.noreply.github.com>
2024-08-22 17:32:14 +0530

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "dev" {
+resource "aws_instance" "name" {
   ami = "ami-0e86e20dae9224db8"
   instance_type = "t2.medium"
   key_name = "susmitha"
